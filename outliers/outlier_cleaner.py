8ffffa4dbaab5e6ee85a33ad31ca9d9ed9854131
fanyaohua <fanyaohua@zhongan.com>
2017-11-01 23:27:27 +0800

    no message

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,22 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
+    all = len(predictions)
+
     ### your code goes here
+    for i in range(all):
+        cleaned_data.append((ages[i][0], net_worths[i][0], abs(predictions[i][0] - net_worths[i][0])))
 
-    
+    print 'old', cleaned_data
+
+    cleaned_data = sorted(cleaned_data, key=lambda item: item[2])
+
+    for j in range(all / 10):
+        cleaned_data.pop()
+
+    print 'new', len(cleaned_data), cleaned_data
+
     return cleaned_data
-
