9534adf7954fc1674062ae15ab1f9a9e1f031180
fanyaohua <fanyaohua@zhongan.com>
2017-11-03 12:44:39 +0800

    no message

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,10 +13,11 @@
 
 import pickle
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -24,8 +25,25 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### your code goes here
 
 
-### your code goes here 
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import precision_score,recall_score
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print 'pred', pred
+
+acc = clf.score(X_test, y_test)
+precision_score(y_test, pred)
+
+print 'X_test', len(X_test), X_test
+print 'y_test', len(y_test), y_test
+print 'acc', acc
+print 'precision_score', precision_score(y_test, pred)
+print 'recall_score', recall_score(y_test, pred)
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,10 +12,11 @@
 
 import pickle
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -25,8 +26,16 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### it's all yours from here forward!
 
 
-### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+from sklearn import tree
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+acc = clf.score(X_test, y_test)
+print acc
